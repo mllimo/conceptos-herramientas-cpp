8704539220af5bc5acff35c8db3b4251965f6d0e
mllimo <ar.guijarro.contreras@gmail.com>
2021-03-17 09:46:13 +0000

    Ignorando ficheros

---
+++ punteros/puntero.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+
+
+int main(int argc, char const *argv[]) {
+  int* puntero_int = new int(2);
+  std::cout << "Direccion de memoria:\t\t\t" << puntero_int << std::endl;
+  std::cout << "Valor de la direccion de memoria:\t" << *puntero_int << std::endl;
+  delete puntero_int;
+  return 0;
+}
