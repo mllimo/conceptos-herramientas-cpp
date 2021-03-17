f0ed451262741dc8fae75dd899a015e8090cfb5a
mllimo <ar.guijarro.contreras@gmail.com>
2021-03-17 10:14:02 +0000

    Subiendo apuntes basicos de makefile

---
+++ makefile/basico/header.cc
@@ -0,0 +1,5 @@
+#include "header.h"
+
+void DecirHola() {
+  std::cout << "hola\n";
+}
---
+++ makefile/basico/main.cc
@@ -0,0 +1,6 @@
+#include "header.h"
+
+int main(int argc, char const *argv[]) {
+  DecirHola();
+  return 0;
+}
